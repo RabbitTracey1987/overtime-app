93384c3e51249413fea4a3db09e3a459ab9ca425
rabbittracey <traceyw0224@gmail.com>
2018-03-16 12:00:28 +0000

    add user validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314014451) do
+ActiveRecord::Schema.define(version: 20180314010522) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 20180314014451) do
     t.string   "username"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do 
+      before do 
+        @user = User.create(email:"test@test.com",password:"123123123",password_confirmation:"123123123", first_name:"tracey",last_name:"Wang")
+      end
+      it "can be createad" do 
+          expect(@user).to be_valid
+      end
+      
+      it "can not be created without first name , last name " do 
+          @user.first_name = nil
+          @user.last_name = nil
+          expect(@user).to_not be_valid
+      end
+  end
 end
