7ad549b5050fab56a4db139166007a3eef47bb2a
rabbittracey <traceyw0224@gmail.com>
2018-05-08 17:47:50 +0000

    add radio button for rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 @user = User.create(email:"traceyw0224@gmail.com",password: "123123123", password_confirmation: "123123123",first_name:"Tracey",last_name:"Wang")
 puts "User created #{@user.first_name}"
+AdminUser.create(email:"admin@gmail.com",password: "123123123", password_confirmation: "123123123",first_name:"Ji",last_name:"Wang")
+puts "AdminUser created #{@user.first_name}"
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{post} rationale content",user_id:@user.id)
 end
